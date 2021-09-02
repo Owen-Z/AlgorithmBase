c6dc8341e6addfa3bcd2a2579c06b0a15a8810f3
Owen-Z <1696685042@qq.com>
2021-09-02 13:37:57 +0800

    Update LRU.cpp
    查找元素时，如果元素是在最后位置，会导致多加一个节点。限制如果后面一个元素为空时，不再添加新元素。

--- LRU/LRU.cpp
+++ LRU/LRU.cpp
@@ -45,13 +45,15 @@ public:
                     searchNode->next->pre = searchNode->pre;
                     searchNode->pre->next = searchNode->next;
                 }
-                Mymap* addNode = (Mymap*) malloc(sizeof (Mymap));
-                addNode->key = key;
-                addNode->value = searchNode->value;
-                addNode->pre = LRU;
-                addNode->next = nullptr;
-                LRU->next = addNode;
-                LRU = addNode;
+                if(searchNode->next != nullptr){
+                    Mymap* addNode = (Mymap*) malloc(sizeof (Mymap));
+                    addNode->key = key;
+                    addNode->value = searchNode->value;
+                    addNode->pre = LRU;
+                    addNode->next = nullptr;
+                    LRU->next = addNode;
+                    LRU = addNode;
+                }
             }else{
                 // Search the place of the first node
                 if(capacity != 1 && searchNode->next != nullptr){
@@ -130,14 +132,14 @@ int main(){
     LRUCache* lRUCache = new LRUCache(2);
 //    lRUCache->put(1, 1); // 缓存是 {1=1}
     lRUCache->put(2, 1); // 缓存是 {1=1, 2=2}
-    lRUCache->get(2);
-    lRUCache->put(2,2);
+    lRUCache->put(3,2);
 //    cout << lRUCache->get(1) << endl;    // 返回 1
 //    lRUCache->put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+    cout << lRUCache->get(3) << endl;    // 返回 -1 (未找到)
     cout << lRUCache->get(2) << endl;    // 返回 -1 (未找到)
-    lRUCache->put(1, 1); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
-    lRUCache->put(4, 1);
-    cout << lRUCache->get(2) << endl;    // 返回 -1 (未找到)
+//    lRUCache->put(1, 1); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+    lRUCache->put(4, 3);
+    cout << lRUCache->get(3) << endl;    // 返回 -1 (未找到)
 //    cout << lRUCache->get(3) << endl;    // 返回 3
 //    cout << lRUCache->get(4) << endl;    // 返回 4
 
